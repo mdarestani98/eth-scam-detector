9b8b3616c83787f25c8655037654a377997b7118
Mohammad <m.darestani98@gmail.com>
2022-10-28 02:58:44 +0800

    [add] first version

---
+++ network.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+from torch import Tensor
+
+
+class FCN(nn.Module):
+    def __init__(self, in_features: int, no_classes: int):
+        super(FCN, self).__init__()
+        self.layer0 = nn.Sequential(
+            nn.Linear(in_features, 32, bias=False),
+            nn.BatchNorm1d(32, momentum=0.9),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        self.layer1 = nn.Sequential(
+            nn.Linear(32, 48, bias=False),
+            nn.BatchNorm1d(48, momentum=0.9),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        self.dropout = nn.Dropout(p=0.1)
+        self.layer2 = nn.Sequential(
+            nn.Linear(48, 64, bias=False),
+            nn.BatchNorm1d(64, momentum=0.9),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(64, 16, bias=False),
+            nn.BatchNorm1d(16, momentum=0.9),
+            nn.ReLU(inplace=True),
+        )
+        self.cls = nn.Linear(16, no_classes, bias=False)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.dropout(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.cls(x)
+        return x
