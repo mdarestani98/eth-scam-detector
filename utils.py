c126c18101a1ab46fcde11ff6a34ec3bb7d53d91
Mohammad <m.darestani98@gmail.com>
2022-10-28 02:58:27 +0800

    [add] avgmeter and ordinal number

--- utils.py
+++ utils.py
@@ -4,6 +4,26 @@ import numpy as np
 from pandas import DataFrame
 
 
+class AverageMeter:
+    def __init__(self):
+        self.value = None
+        self.sum = None
+        self.count = 0
+        self.average = None
+
+    def update(self, data, count: int = 1):
+        self.value = data
+        if self.sum is None:
+            self.sum = data * count
+        else:
+            self.sum += data * count
+        self.count += count
+        self.average = self.sum / self.count
+
+    def reset(self):
+        self.value, self.sum, self.count, self.average = None, None, 0, None
+
+
 def find_highly_correlated(corr: DataFrame, threshold: float = 0.9) -> List[Tuple[str, str]]:
     mask = np.zeros_like(corr)
     mask[np.triu_indices_from(mask, k=1)] = True
@@ -28,3 +48,7 @@ def find_least_correlated_with(pairs: List[Tuple[str, str]], corr: DataFrame, ta
         else:
             must_delete.append(pair[1])
     return must_delete
+
+
+def ordinal(n: int) -> str:
+    return "%d%s" % (n, "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
